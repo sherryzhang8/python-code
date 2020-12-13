528010ab9062924c291dde9d0adb1f80b458182b
Sherryzhang8 <54751217+sherryzhang8@users.noreply.github.com>
2020-12-13 11:57:16 -0800

    Create sockMerchant.py

---
+++ sherry/sockMerchant.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+ #   ar_list = ar.split()
+    color_list = []
+    total_cnt = 0
+    for i in range(0,n-1):
+        pair = ar[i]
+        if pair not in color_list:
+            color_list.append(pair)
+            cnt = 1
+            for j in range(i+1,n-1):
+                if pair == ar[j]:
+                    cnt += 1
+            total_cnt += int(cnt/2)
+    return total_cnt   
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
